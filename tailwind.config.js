8c7610546ac158671f57a87f6fed69231adffbb0
Alberto Sánchez <albersanchez511@hotmail.com>
2023-10-09 12:50:35 +0200

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif']
+      },
+      colors: {
+        customWhite: '#F2F5F9',
+        customBlue: '#3662E3',
+        customNavyBlue: '#111729',
+        customGray: '#364153'
+      }
+    }
+  },
+  plugins: []
+}

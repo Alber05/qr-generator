fb8bf390438f964e045f039b0287ea47ee5d04bd
Alberto Sánchez <albersanchez511@hotmail.com>
2023-10-10 10:01:36 +0200

    Fix download

--- src/pages/QrCodePage.jsx
+++ src/pages/QrCodePage.jsx
@@ -8,11 +8,23 @@ import clipboardCopy from 'clipboard-copy'
 function QrCodePage({ url }) {
   const qrCodeRef = useRef(null)
 
-  console.log(qrCodeRef)
-
   const downloadQRCode = () => {
-    const qrCodeCanvas = qrCodeRef.current.props
-    console.log(qrCodeCanvas)
+    const qrCodeCanvas = qrCodeRef.current
+    const svgData = new XMLSerializer().serializeToString(qrCodeCanvas)
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+    img.onload = () => {
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx.drawImage(img, 0, 0)
+      const pngFile = canvas.toDataURL('image/png')
+      const downloadLink = document.createElement('a')
+      downloadLink.download = 'QRCode'
+      downloadLink.href = `${pngFile}`
+      downloadLink.click()
+    }
+    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`
   }
 
   const copyToClipboard = (url) => {
